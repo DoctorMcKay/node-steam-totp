d78ed7fa9796126d21f33f312401dabd55919e9f
Alexander Corn <mckay@doctormckay.com>
2015-12-02 14:11:26 -0500

    Added getConfirmationKey

--- index.js
+++ index.js
@@ -34,6 +34,38 @@ exports.generateAuthCode = function(secret, timeOffset) {
 	return code;
 };
 
+/**
+ * Generate a base64 confirmation key for use with mobile trade confirmations. The key can only be used once.
+ * @param {Buffer|string} identitySecret - The identity_secret that you received when enabling two-factor authentication
+ * @param {number} time - The Unix time for which you are generating this secret. Generally should be the current time.
+ * @param {string} tag - The tag which identifies what this request (and therefore key) will be for. "conf" to load the confirmations page, "details" to load details about a trade, "allow" to confirm a trade, "cancel" to cancel it.
+ * @returns {string}
+ */
+exports.getConfirmationKey = function(identitySecret, time, tag) {
+	identitySecret = bufferizeSecret(identitySecret);
+
+	var dataLen = 8;
+
+	if(tag) {
+		if(tag.length > 32) {
+			dataLen += 32;
+		} else {
+			dataLen += tag.length;
+		}
+	}
+
+	var buffer = new Buffer(dataLen);
+	buffer.writeUInt32BE(0, 0); // This will stop working in 2038!
+	buffer.writeUInt32BE(time, 4);
+
+	if(tag) {
+		buffer.write(tag, 8);
+	}
+
+	var hmac = Crypto.createHmac('sha1', identitySecret);
+	return hmac.update(buffer).digest('base64');
+};
+
 function bufferizeSecret(secret) {
 	if(typeof secret === 'string') {
 		// Check if it's hex
