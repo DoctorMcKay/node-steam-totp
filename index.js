2e6c4831f2ec1468fe03e3bf4cdeab0c1c96932b
Kysune <szymonlisowiec@gmail.com>
2018-04-03 07:52:55 -0400

    Salt for getDeviceID
    What do you think about add salt to `getDeviceID` in order to complicate detection a trade bot by Steam.
    
    (cherry picked from commit e7f0232)

--- index.js
+++ index.js
@@ -143,7 +143,8 @@ exports.getTimeOffset = function(callback) {
  * @returns {string}
  */
 exports.getDeviceID = function(steamID) {
-	return "android:" + Crypto.createHash('sha1').update(steamID.toString()).digest('hex')
+	let salt = global._steam_totp_salt || '';
+	return "android:" + Crypto.createHash('sha1').update(steamID.toString() + salt).digest('hex')
 			.replace(/^([0-9a-f]{8})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{12}).*$/, '$1-$2-$3-$4-$5');
 };
 
